709f5d82ead52cf579cdab27556ab9448c4c58ff
AngelaFig <angelafigueroa596@gmail.com>
2023-10-20 12:49:40 -0400

    added routes to main.jsx, added Outlet logic to app.jsx, created NavTab with use of link and, use location

--- React Portfolio/src/main.jsx
+++ React Portfolio/src/main.jsx
@@ -4,19 +4,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.jsx'
 // import './index.css'
+import Error from './pages/Error';
+import Home from './pages/Home'
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
-
-
-
-
-
-
-
-
-
-
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/AboutMe',
+        element: <AboutMe />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <App />
+   <RouterProvider router={router} />
 )
