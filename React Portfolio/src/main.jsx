606cf0d68f5da726fedfccdbb261181701205138
AngelaFig <angelafigueroa596@gmail.com>
2023-10-20 12:30:57 -0400

    added components and pages folders. Added aboutme, contact, error, portfolio, and resume to pages. Added logic to error.jsx

--- React Portfolio/src/main.jsx
+++ React Portfolio/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+
+
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <App />
 )
