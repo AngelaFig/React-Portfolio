ec92cb9944d74c3283e1e21374f886f0e9ac1d16
AngelaFig <angelafigueroa596@gmail.com>
2023-10-25 23:08:51 -0400

    added about me

--- React Portfolio/src/pages/AboutMe.jsx
+++ React Portfolio/src/pages/AboutMe.jsx
@@ -3,7 +3,7 @@
 function About () {
     return (
         <div>
-            <h2>About Me</h2>
+            <h2>I am currently completing a Full Stack web development bootcamp with UPenn LPS. I graduated from William Paterson University in January of 2023 with a bachelor's degree in Philosophy.</h2>
         </div>
     )
 }
